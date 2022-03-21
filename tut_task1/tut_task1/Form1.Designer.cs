ce1eafd5d3672575ebe317f7e3a20b270acc750a
nhscomputing2021 <81550918+nhscomputing2021@users.noreply.github.com>
2022-03-22 10:36:44 +1100

    Add files via upload

---
+++ tut_task1/tut_task1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace tut_task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblOuput = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.comCat = new System.Windows.Forms.ComboBox();
+            this.numAge = new System.Windows.Forms.NumericUpDown();
+            this.numPurchasedPrice = new System.Windows.Forms.NumericUpDown();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPurchasedPrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Item Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(436, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(144, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "How old is this item?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(436, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "How much did you pay for it?";
+            // 
+            // lblOuput
+            // 
+            this.lblOuput.AutoSize = true;
+            this.lblOuput.Location = new System.Drawing.Point(144, 324);
+            this.lblOuput.Name = "lblOuput";
+            this.lblOuput.Size = new System.Drawing.Size(51, 20);
+            this.lblOuput.TabIndex = 4;
+            this.lblOuput.Text = "label5";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(148, 89);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 5;
+            // 
+            // comCat
+            // 
+            this.comCat.FormattingEnabled = true;
+            this.comCat.Items.AddRange(new object[] {
+            "Laptops",
+            "Calculators",
+            "Gaming devices",
+            "Computer parts"});
+            this.comCat.Location = new System.Drawing.Point(440, 86);
+            this.comCat.Name = "comCat";
+            this.comCat.Size = new System.Drawing.Size(121, 28);
+            this.comCat.TabIndex = 6;
+            // 
+            // numAge
+            // 
+            this.numAge.Location = new System.Drawing.Point(148, 190);
+            this.numAge.Name = "numAge";
+            this.numAge.Size = new System.Drawing.Size(120, 26);
+            this.numAge.TabIndex = 7;
+            // 
+            // numPurchasedPrice
+            // 
+            this.numPurchasedPrice.DecimalPlaces = 1;
+            this.numPurchasedPrice.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numPurchasedPrice.Location = new System.Drawing.Point(440, 190);
+            this.numPurchasedPrice.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.numPurchasedPrice.Name = "numPurchasedPrice";
+            this.numPurchasedPrice.Size = new System.Drawing.Size(120, 26);
+            this.numPurchasedPrice.TabIndex = 8;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(148, 246);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 47);
+            this.btnCalculate.TabIndex = 9;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(440, 246);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(125, 47);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "End the quote";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 526);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.numPurchasedPrice);
+            this.Controls.Add(this.numAge);
+            this.Controls.Add(this.comCat);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblOuput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPurchasedPrice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblOuput;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ComboBox comCat;
+        private System.Windows.Forms.NumericUpDown numAge;
+        private System.Windows.Forms.NumericUpDown numPurchasedPrice;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ tut_task1/tut_task1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tut_task1
+{
+    public partial class Form1 : Form
+    {
+        float total = 0f;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resetInputs()
+        {
+            txtName.Text = "";
+            comCat.ResetText();
+            numAge.Value = 0;
+            numPurchasedPrice.Value = 0;
+        }
+
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int age = (int)numAge.Value;
+            float purchasedPrice = (float)numPurchasedPrice.Value;
+            
+
+            if(age == 0 | purchasedPrice == 0.0)
+            {
+                lblOuput.Text = "Please enter a valid price or year";
+            }
+            else
+            {
+                float currentValue = CalculateCurrentValue(purchasedPrice, age);
+                total += currentValue;
+                lblOuput.Text = $"The item is worth {currentValue}.\n The collection so far is worth {total}";
+            }
+
+            resetInputs();
+
+
+
+        }
+
+        private float CalculateCurrentValue(float purchasedValue, int age)
+        {
+            float depreciation = purchasedValue * 0.2f * age;
+            if (depreciation > purchasedValue) return 0f;
+            return purchasedValue - depreciation;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //resetInputs();
+            lblOuput.Text = "";
+        }
+    }
+}
